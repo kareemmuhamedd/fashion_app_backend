eb10fac95cfb187159b2e00237b687f7b6d290d5
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2024-09-12 20:11:34 +0300

    - Added wishlist app. - Registered wishlist app in main backend settings.py. - Added wishlist model. - Added wishlist serializers for serialize product data model. - Registered wishlist model in admin.

---
+++ fashion_backend/wishlist/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from . import models
+
+
+class WishListSerializers(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source='product.id')
+    title = serializers.ReadOnlyField(source='product.title')
+    description = serializers.ReadOnlyField(source='product.description')
+    is_featured = serializers.ReadOnlyField(source='product.is_featured')
+    clothesType = serializers.ReadOnlyField(source='product.clothesType')
+    ratings = serializers.ReadOnlyField(source='product.ratings')
+    category = serializers.ReadOnlyField(source='product.category')
+    brand = serializers.ReadOnlyField(source='product.brand')
+    colors = serializers.ReadOnlyField(source='product.colors')
+    sizes = serializers.ReadOnlyField(source='product.sizes')
+    imageUrls = serializers.ReadOnlyField(source='product.imageUrls')
+    created_at = serializers.ReadOnlyField(source='product.created_at')
+
+    class Meta:
+        model = models.WishList
+        fields = ['id', 'title', 'description', 'is_featured', 'clothesType',
+                  'ratings', 'category', 'colors', 'sizes', 'imageUrls', 'created_at']
