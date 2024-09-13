264326f5c3e7af6b335999a1a3ac6598bff41924
kareemmuhamedd <kareemmuhamed2g@gmail.com>
2024-09-13 14:35:37 +0300

    - Added wishlist views: GetWishList and ToggleWishList. - Added wishlist URL and hooked it into the core urls file. - Added three missing variables in the wishlist serializers: category, brand, and price.

--- fashion_backend/wishlist/serializers.py
+++ fashion_backend/wishlist/serializers.py
@@ -5,12 +5,13 @@ from . import models
 class WishListSerializers(serializers.ModelSerializer):
     id = serializers.ReadOnlyField(source='product.id')
     title = serializers.ReadOnlyField(source='product.title')
+    price = serializers.ReadOnlyField(source='product.price')
     description = serializers.ReadOnlyField(source='product.description')
     is_featured = serializers.ReadOnlyField(source='product.is_featured')
     clothesType = serializers.ReadOnlyField(source='product.clothesType')
     ratings = serializers.ReadOnlyField(source='product.ratings')
-    category = serializers.ReadOnlyField(source='product.category')
-    brand = serializers.ReadOnlyField(source='product.brand')
+    category = serializers.ReadOnlyField(source='product.category.id')
+    brand = serializers.ReadOnlyField(source='product.brand.id')
     colors = serializers.ReadOnlyField(source='product.colors')
     sizes = serializers.ReadOnlyField(source='product.sizes')
     imageUrls = serializers.ReadOnlyField(source='product.imageUrls')
@@ -19,4 +20,4 @@ class WishListSerializers(serializers.ModelSerializer):
     class Meta:
         model = models.WishList
         fields = ['id', 'title', 'description', 'is_featured', 'clothesType',
-                  'ratings', 'category', 'colors', 'sizes', 'imageUrls', 'created_at']
+                  'ratings', 'category', 'colors', 'sizes', 'imageUrls', 'created_at', 'brand', 'price']
--- fashion_backend/wishlist/views.py
+++ fashion_backend/wishlist/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from . import models, serializers
 
-# Create your views here.
+
+class GetWishList(generics.ListAPIView):
+    serializer_class = serializers.WishListSerializers
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return models.WishList.objects.filter(userId=self.request.user)
+
+
+class ToggleWishList(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user_id = request.user.id
+        product_id = request.query_params.get('id')
+
+        if not user_id or not product_id:
+            return Response({'message': 'Invalid Request a user id and product id is required'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            product = models.Product.objects.get(id=product_id)
+        except models.Product.DoesNotExist:
+            return Response({'message': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        wishlist_item, created = models.WishList.objects.get_or_create(
+            userId=request.user, product=product)
+        if created:
+            return Response({'message': 'Product added to wishlist'}, status=status.HTTP_201_CREATED)
+        else:
+            wishlist_item.delete()
+            return Response({'message': 'Product removed from wishlist'}, status=status.HTTP_204_NO_CONTENT)
